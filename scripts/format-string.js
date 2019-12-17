36ca4899aabf469bb69fd7290510f033c98afcc4
davidmazi <david.maziere@gmail.com>
2019-12-17 18:47:53 +0100

    Removes Initiazilation from individual files

--- scripts/format-string.js
+++ scripts/format-string.js
@@ -1,165 +1,167 @@
 var dicoOp = new Map();
-initDicoOp();
 
 /**
  * Initialise le dictionnaire des opérations possibles
  */
 function initDicoOp() {
-    // ----------------Symboles----------------
-    dicoOp.set("un", "1");
-    dicoOp.set("deux", "2");
+  // ----------------Symboles----------------
+  dicoOp.set("un", "1");
+  dicoOp.set("deux", "2");
 
-    dicoOp.set("plus"  , "+");
-    dicoOp.set("moins", "-");
+  dicoOp.set("plus", "+");
+  dicoOp.set("moins", "-");
 
-    dicoOp.set("égal"  , "=");
-    dicoOp.set("égals"  , "=");
-    dicoOp.set("égale" , "=");
-    dicoOp.set("égales" , "=");
-    dicoOp.set("égalent", "=");
-    
-    dicoOp.set("factoriel", "!");
-    dicoOp.set("factoriels", "!");
-    dicoOp.set("factorielle", "!");
-    dicoOp.set("factorielles", "!");
+  dicoOp.set("égal", "=");
+  dicoOp.set("égals", "=");
+  dicoOp.set("égale", "=");
+  dicoOp.set("égales", "=");
+  dicoOp.set("égalent", "=");
 
-    dicoOp.set("différent"  , "ne");
-    dicoOp.set("inégal", "ne");
-    dicoOp.set("inégals", "ne");
-    dicoOp.set("inégale", "ne");
-    dicoOp.set("inégales", "ne");
-    dicoOp.set("inégalent", "ne");
+  dicoOp.set("factoriel", "!");
+  dicoOp.set("factoriels", "!");
+  dicoOp.set("factorielle", "!");
+  dicoOp.set("factorielles", "!");
 
-    dicoOp.set("pi", "pi");
+  dicoOp.set("différent", "ne");
+  dicoOp.set("inégal", "ne");
+  dicoOp.set("inégals", "ne");
+  dicoOp.set("inégale", "ne");
+  dicoOp.set("inégales", "ne");
+  dicoOp.set("inégalent", "ne");
 
-    dicoOp.set("multiplié", "fois");
-    dicoOp.set("multipliés", "fois");
-    dicoOp.set("multipliée", "fois");
-    dicoOp.set("multipliées", "fois");
-    dicoOp.set("multiplie", "fois");
-    dicoOp.set("multiplies", "fois");
-    dicoOp.set("multiplient", "fois");
+  dicoOp.set("pi", "pi");
 
-    dicoOp.set("divisé", "div");
-    dicoOp.set("divisés", "div");
-    dicoOp.set("divisée", "div");
-    dicoOp.set("divisées", "div");
-    dicoOp.set("divise", "div");
-    dicoOp.set("divises", "div");
-    dicoOp.set("divisent", "div");
+  dicoOp.set("multiplié", "fois");
+  dicoOp.set("multipliés", "fois");
+  dicoOp.set("multipliée", "fois");
+  dicoOp.set("multipliées", "fois");
+  dicoOp.set("multiplie", "fois");
+  dicoOp.set("multiplies", "fois");
+  dicoOp.set("multiplient", "fois");
 
-    dicoOp.set("sur", "sur");
-    dicoOp.set("sûr", "sur");
-    dicoOp.set("sûrs", "sur");
-    dicoOp.set("sûre", "sur");
-    dicoOp.set("sûres", "sur");
+  dicoOp.set("divisé", "div");
+  dicoOp.set("divisés", "div");
+  dicoOp.set("divisée", "div");
+  dicoOp.set("divisées", "div");
+  dicoOp.set("divise", "div");
+  dicoOp.set("divises", "div");
+  dicoOp.set("divisent", "div");
 
-    dicoOp.set("parenthèse", "parenthese");
-    dicoOp.set("parenthèses", "parenthese");
-    dicoOp.set("crochet","crochet");
+  dicoOp.set("sur", "sur");
+  dicoOp.set("sûr", "sur");
+  dicoOp.set("sûrs", "sur");
+  dicoOp.set("sûre", "sur");
+  dicoOp.set("sûres", "sur");
 
-    dicoOp.set("ouvert", "open");
-    dicoOp.set("ouverts", "open");
-    dicoOp.set("ouverte", "open");
-    dicoOp.set("ouvertes", "open");
-    dicoOp.set("ouvrant", "open");
-    dicoOp.set("ouvrante", "open");
-    dicoOp.set("ouvrantes", "open");
+  dicoOp.set("parenthèse", "parenthese");
+  dicoOp.set("parenthèses", "parenthese");
+  dicoOp.set("crochet", "crochet");
 
-    dicoOp.set("fermé", "closed");
-    dicoOp.set("fermés", "closed");
-    dicoOp.set("fermée", "closed");
-    dicoOp.set("fermées", "closed");
-    dicoOp.set("fermant", "closed");
-    dicoOp.set("fermante", "closed");
-    dicoOp.set("fermantes", "closed");
+  dicoOp.set("ouvert", "open");
+  dicoOp.set("ouverts", "open");
+  dicoOp.set("ouverte", "open");
+  dicoOp.set("ouvertes", "open");
+  dicoOp.set("ouvrant", "open");
+  dicoOp.set("ouvrante", "open");
+  dicoOp.set("ouvrantes", "open");
 
-    dicoOp.set("index", "index");
-    dicoOp.set("indexe", "index");
-    dicoOp.set("indexes", "index");
-    dicoOp.set("indexent", "index");
-    dicoOp.set("indexé", "index");
-    dicoOp.set("indexés", "index");
-    dicoOp.set("indexée", "index");
-    dicoOp.set("indexées", "index");
+  dicoOp.set("fermé", "closed");
+  dicoOp.set("fermés", "closed");
+  dicoOp.set("fermée", "closed");
+  dicoOp.set("fermées", "closed");
+  dicoOp.set("fermant", "closed");
+  dicoOp.set("fermante", "closed");
+  dicoOp.set("fermantes", "closed");
 
-    dicoOp.set("indice", "indice");
-    dicoOp.set("indices", "indice");
+  dicoOp.set("index", "index");
+  dicoOp.set("indexe", "index");
+  dicoOp.set("indexes", "index");
+  dicoOp.set("indexent", "index");
+  dicoOp.set("indexé", "index");
+  dicoOp.set("indexés", "index");
+  dicoOp.set("indexée", "index");
+  dicoOp.set("indexées", "index");
 
-    dicoOp.set("puissance", "puissance");
-    dicoOp.set("puissances", "puissance");
+  dicoOp.set("indice", "indice");
+  dicoOp.set("indices", "indice");
 
-    dicoOp.set("intégral", "intégrale");
-    dicoOp.set("intégrals", "intégrale");
-    dicoOp.set("intégrale", "intégrale");
-    dicoOp.set("intégrales", "intégrale");
+  dicoOp.set("puissance", "puissance");
+  dicoOp.set("puissances", "puissance");
 
-    dicoOp.set("somme", "somme");
-    dicoOp.set("sommes", "somme");
+  dicoOp.set("intégral", "intégrale");
+  dicoOp.set("intégrals", "intégrale");
+  dicoOp.set("intégrale", "intégrale");
+  dicoOp.set("intégrales", "intégrale");
 
-    // ----------------Expressions----------------
-    dicoOp.set("racine", "racine");
-    dicoOp.set("racines", "racine");
+  dicoOp.set("somme", "somme");
+  dicoOp.set("sommes", "somme");
 
-    dicoOp.set("fraction", "fraction");
-    dicoOp.set("fractions", "fraction");
-    dicoOp.set("fractionne", "fraction");
-    dicoOp.set("fractionnes", "fraction");
-    dicoOp.set("fractionnent", "fraction");
-    dicoOp.set("fractionné", "fraction");
-    dicoOp.set("fractionnés", "fraction");
-    dicoOp.set("fractionnée", "fraction");
-    dicoOp.set("fractionnées", "fraction");
+  // ----------------Expressions----------------
+  dicoOp.set("racine", "racine");
+  dicoOp.set("racines", "racine");
 
-    dicoOp.set("à", "à");
-    dicoOp.set("a", "à");
+  dicoOp.set("fraction", "fraction");
+  dicoOp.set("fractions", "fraction");
+  dicoOp.set("fractionne", "fraction");
+  dicoOp.set("fractionnes", "fraction");
+  dicoOp.set("fractionnent", "fraction");
+  dicoOp.set("fractionné", "fraction");
+  dicoOp.set("fractionnés", "fraction");
+  dicoOp.set("fractionnée", "fraction");
+  dicoOp.set("fractionnées", "fraction");
 
-    // ----------------Séquenceurs----------------
-    dicoOp.set("stop", "stop");
-    dicoOp.set("stopper", "stop");
-    dicoOp.set("stoppe", "stop");
-    dicoOp.set("stoppes", "stop");
-    dicoOp.set("stoppent", "stop");
-    dicoOp.set("stoppé", "stop");
-    dicoOp.set("stoppés", "stop");
-    dicoOp.set("stoppée", "stop");
-    dicoOp.set("stoppées", "stop");
+  dicoOp.set("à", "à");
+  dicoOp.set("a", "à");
+
+  // ----------------Séquenceurs----------------
+  dicoOp.set("stop", "stop");
+  dicoOp.set("stopper", "stop");
+  dicoOp.set("stoppe", "stop");
+  dicoOp.set("stoppes", "stop");
+  dicoOp.set("stoppent", "stop");
+  dicoOp.set("stoppé", "stop");
+  dicoOp.set("stoppés", "stop");
+  dicoOp.set("stoppée", "stop");
+  dicoOp.set("stoppées", "stop");
 }
 
-
 /**
- * Pour une équation transcrite (oral à écrit), 
- * formate le string en fonction des opérateurs contenus dans le dictionnaire 
+ * Pour une équation transcrite (oral à écrit),
+ * formate le string en fonction des opérateurs contenus dans le dictionnaire
  * et renvoie une équation formatée (tableau de string)
- * 
+ *
  * @param {string} rawEquation L'équation à formater
  * @returns {string[]} L'équation formatée
  */
 function format(rawEquation) {
-    var rawEquationTab = rawEquation.toLowerCase().split(" ");
-    var formatEquation = [];
-    var parentheseOrCrochet = "";
-    for (var i = 0; i < rawEquationTab.length; i++) {
-        var equationElem = rawEquationTab[i];
-        if (isNaN(equationElem)){
-            if(dicoOp.get(rawEquationTab[i])=="parenthese")
-                parentheseOrCrochet = "p";
-            else if(dicoOp.get(rawEquationTab[i])=="crochet")
-                parentheseOrCrochet = "c";
-            else{
-                if(parentheseOrCrochet.length!=0 && (dicoOp.get(rawEquationTab[i])=="open" || dicoOp.get(rawEquationTab[i])=="closed")){
-                    equationElem = parentheseOrCrochet+dicoOp.get(rawEquationTab[i]);
-                }
-                else{
-                    equationElem = dicoOp.get(rawEquationTab[i]);
-                }                    
-                parentheseOrCrochet = "";
-            }
+  var rawEquationTab = rawEquation.toLowerCase().split(" ");
+  var formatEquation = [];
+  var parentheseOrCrochet = "";
+  for (var i = 0; i < rawEquationTab.length; i++) {
+    var equationElem = rawEquationTab[i];
+    if (isNaN(equationElem)) {
+      if (dicoOp.get(rawEquationTab[i]) == "parenthese")
+        parentheseOrCrochet = "p";
+      else if (dicoOp.get(rawEquationTab[i]) == "crochet")
+        parentheseOrCrochet = "c";
+      else {
+        if (
+          parentheseOrCrochet.length != 0 &&
+          (dicoOp.get(rawEquationTab[i]) == "open" ||
+            dicoOp.get(rawEquationTab[i]) == "closed")
+        ) {
+          equationElem = parentheseOrCrochet + dicoOp.get(rawEquationTab[i]);
+        } else {
+          equationElem = dicoOp.get(rawEquationTab[i]);
         }
-            
-        if (equationElem != undefined) {
-            formatEquation.push(equationElem);
-        }
+        parentheseOrCrochet = "";
+      }
     }
-    return formatEquation;
+
+    if (equationElem != undefined) {
+      formatEquation.push(equationElem);
+    }
+  }
+  console.log(`Format Equation : ${formatEquation}`);
+  return formatEquation;
 }
--- scripts/latex-string.js
+++ scripts/latex-string.js
@@ -1,49 +1,40 @@
 var dicoSymboleLatex = new Map();
 var dicoExpressionLatex = new Map();
 var dicoSequenceurLatex = new Map();
-initDicoSymboleLatex();
-initDicoExpressionLatex();
-initDicoSequenceurLatex();
-var eq0 = ["intégrale","index","i","=","1","à","n","stop","somme","index","i","=","1","à","n","stop","fraction","13","fois","x","puissance","8","sur","racine","7","indice","2"];
-var eq1 = ["somme","index","i","=","1","à","n"];
-var eq2 = ["fraction","13","fois","x","puissance","8","sur","racine","7","indice","2"];
-latex(eq0);
-
 
 function initDicoSymboleLatex() {
-    dicoSymboleLatex.set("un", "1");
-    dicoSymboleLatex.set("deux", "2");
-    dicoSymboleLatex.set("trois", "3");
-    dicoSymboleLatex.set("quatre", "4");
-    dicoSymboleLatex.set("cinq", "5");
-    dicoSymboleLatex.set("six", "6");
-    dicoSymboleLatex.set("sept", "7");
-    dicoSymboleLatex.set("huit", "8");
-    dicoSymboleLatex.set("neuf", "9");
+  dicoSymboleLatex.set("un", "1");
+  dicoSymboleLatex.set("deux", "2");
+  dicoSymboleLatex.set("trois", "3");
+  dicoSymboleLatex.set("quatre", "4");
+  dicoSymboleLatex.set("cinq", "5");
+  dicoSymboleLatex.set("six", "6");
+  dicoSymboleLatex.set("sept", "7");
+  dicoSymboleLatex.set("huit", "8");
+  dicoSymboleLatex.set("neuf", "9");
 
-    dicoSymboleLatex.set("ne", "\\ne");
-    dicoSymboleLatex.set("pi", "\\pi");
-    dicoSymboleLatex.set("fois", "*");
-    dicoSymboleLatex.set("div", "\\div");
-    dicoSymboleLatex.set("sur", "}{");
-    dicoSymboleLatex.set("pOpen", "(");
-    dicoSymboleLatex.set("pClosed", ")");
-    dicoSymboleLatex.set("cOpen", "[");
-    dicoSymboleLatex.set("cClosed", "]");
-    dicoSymboleLatex.set("index", "_{");
-    dicoSymboleLatex.set("indice", "_");
-    dicoSymboleLatex.set("puissance", "^");
-    dicoSymboleLatex.set("intégrale", "\\int");
-    dicoSymboleLatex.set("somme", "\\sum");
+  dicoSymboleLatex.set("ne", "\\ne");
+  dicoSymboleLatex.set("pi", "\\pi");
+  dicoSymboleLatex.set("fois", "*");
+  dicoSymboleLatex.set("div", "\\div");
+  dicoSymboleLatex.set("sur", "}{");
+  dicoSymboleLatex.set("pOpen", "(");
+  dicoSymboleLatex.set("pClosed", ")");
+  dicoSymboleLatex.set("cOpen", "[");
+  dicoSymboleLatex.set("cClosed", "]");
+  dicoSymboleLatex.set("index", "_{");
+  dicoSymboleLatex.set("indice", "_");
+  dicoSymboleLatex.set("puissance", "^");
+  dicoSymboleLatex.set("intégrale", "\\int");
+  dicoSymboleLatex.set("somme", "\\sum");
 }
 function initDicoExpressionLatex() {
-    dicoExpressionLatex.set("racine", "\\sqrt{");
-    dicoExpressionLatex.set("fraction", "\\frac{");
-    dicoExpressionLatex.set("à", "}^{");
+  dicoExpressionLatex.set("racine", "\\sqrt{");
+  dicoExpressionLatex.set("fraction", "\\frac{");
+  dicoExpressionLatex.set("à", "}^{");
 }
 function initDicoSequenceurLatex() {
-    dicoSequenceurLatex.set("stop", "}");
-
+  dicoSequenceurLatex.set("stop", "}");
 }
 
 //["1","plus","3","racine","2","div","pi"]
@@ -52,37 +43,33 @@ function initDicoSequenceurLatex() {
 /**
  * Pour une équation formatée (tableau de string),
  * retourne l'équation écrite en LaTeX
- * 
+ *
  * @param {string[]} formatEquation L'équation formatée
  * @returns {string} L'équation en Latex
  */
 function latex(formatEquation) {
-    var latexEquation = "";
-    var compteurAccoladesFin = 0;
-    for (var i = 0; i < formatEquation.length; i++) {
-        var equationElem = formatEquation[i];       
-        if(dicoSymboleLatex.has(equationElem)){
-            latexEquation += dicoSymboleLatex.get(formatEquation[i]);
-        }
-        else if(dicoExpressionLatex.has(equationElem)){
-            latexEquation += dicoExpressionLatex.get(formatEquation[i]);
-            compteurAccoladesFin++;
-        }
-        else if(dicoSequenceurLatex.has(equationElem)){
-            latexEquation += dicoSequenceurLatex.get(formatEquation[i]);
-            compteurAccoladesFin--;
-        }
-        else if(equationElem.length == 1 || !isNaN(equationElem)){
-            latexEquation += equationElem;
-        }
-        else{
-            latexEquation += "N/A"
-        }                
+  var latexEquation = "";
+  var compteurAccoladesFin = 0;
+  for (var i = 0; i < formatEquation.length; i++) {
+    var equationElem = formatEquation[i];
+    if (dicoSymboleLatex.has(equationElem)) {
+      latexEquation += dicoSymboleLatex.get(formatEquation[i]);
+    } else if (dicoExpressionLatex.has(equationElem)) {
+      latexEquation += dicoExpressionLatex.get(formatEquation[i]);
+      compteurAccoladesFin++;
+    } else if (dicoSequenceurLatex.has(equationElem)) {
+      latexEquation += dicoSequenceurLatex.get(formatEquation[i]);
+      compteurAccoladesFin--;
+    } else if (equationElem.length == 1 || !isNaN(equationElem)) {
+      latexEquation += equationElem;
+    } else {
+      latexEquation += "N/A";
     }
-    while(compteurAccoladesFin>0){
-        latexEquation += "}";
-        compteurAccoladesFin--;
-    }
-    console.log(latexEquation);
-    return latexEquation;
+  }
+  while (compteurAccoladesFin > 0) {
+    latexEquation += "}";
+    compteurAccoladesFin--;
+  }
+  console.log(`Equation LateX : ${latexEquation}`);
+  return latexEquation;
 }
